8cd45ed5c55661b764e21b8664f97fc388210216
Kirill <d.str1ke@yandex.ru>
2021-12-02 04:09:42 +0500

    modal + responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const modal = document.querySelector('.modal')
+    const btnmodal = document.querySelector('#sumModal');
+    btnmodal.addEventListener('click', ()=>{
+        modal.classList.remove('hide');
+        modal.classList.add('show');
+        document.body.style.overflow = 'hidden';
+    })
+    
+    modal.addEventListener('click', (e) => {
+        if (e.target === document.querySelector('.modal__bg') ||  e.target.getAttribute('data-close') == '') {
+            hideModal();
+        }
+    })
+
+    function hideModal() {
+        modal.classList.remove('show');
+        modal.classList.add('hide');
+        document.body.style.overflow = '';
+    }
+  });
